c8c1e89301269d63907c009e0c3b2965f151ec2a
k4rth1 <karthikeyanks463@gmail.com>
2024-10-01 22:32:42 +0530

    Revert "Add Login & Register design"
    This reverts commit 649021d598aa6c113808408935deea8285e73486

--- app/src/main/java/com/student_developer/track_my_grade/LoginActivity.java
+++ app/src/main/java/com/student_developer/track_my_grade/LoginActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
